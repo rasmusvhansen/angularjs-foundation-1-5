7b9b506f366d2444420e65fed3d366952784655b
Rasmus Vestergaard Hansen <rasmusvhansen@gmail.com>
2016-03-03 14:54:28 +0000

    added component exercise

--- exercises/04Components/app.js
+++ exercises/04Components/app.js
@@ -1,10 +1,16 @@
-/*(function() {
+(function() {
   'use strict';
+  
+  // TODO refactor this app into two components: an app component and a videoDetail component
+  // see https://docs.angularjs.org/guide/component for help
+  // 1. Create an app component that has a controller similar to the VideoListController - put template in app.html
+  // 2. Create a videoDetail component which is responsible for rendering a single video - put template in videoDetails.html
+  // 3. Change index.html to use the app component
+  // 4. For bonus points, extract a search component from the app component and use output parameters on that to have app component orchestrate search and render
+  
   angular.module('myApp', []).controller('VideoListController', function($http, $log) {
     var ctrl = this;
     ctrl.query = 'Rick Astley';
-    
-    
     
     ctrl.search = function() {
       return $http.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&type=video&q=' + ctrl.query + '&key=AIzaSyD4YJITOWdfQdFbcxHc6TgeCKmVS9yRuQ8')
@@ -25,4 +31,4 @@
     
     ctrl.search();
   });
-})();*/
+})();
