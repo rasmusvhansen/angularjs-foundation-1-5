1f685320e08c49b9ac0fceee15fdb935148af16e
Rasmus Vestergaard Hansen <rasmusvhansen@gmail.com>
2016-03-04 14:53:43 +0000

    Misc updates

--- exercises/05Services/scripts/routing.js
+++ exercises/05Services/scripts/routing.js
@@ -3,18 +3,18 @@
   .config(function($stateProvider, $urlRouterProvider) {
     //
     // For any unmatched url, redirect to /video
-    $urlRouterProvider.otherwise("/video");
+    $urlRouterProvider.otherwise('/video');
     //
     // Now set up the states
     $stateProvider.state('video', {
-      url: "/video",
-      templateUrl: "templates/video.html",
+      url: '/video',
+      templateUrl: 'templates/video.html',
       controller: 'SearchController as searchController'
     })
       
       .state('video.list', {
-      url: "/list/:query",
-      templateUrl: "templates/video.list.html",
+      url: '/list/:query',
+      templateUrl: 'templates/video.list.html',
       // TODO instead of resolving query, use videoService to resolve a list of videos
       resolve: {
         query: function($stateParams) {
--- samples/05Routing/app.js
+++ samples/05Routing/app.js
@@ -1,23 +1,26 @@
 (function() {
   'use strict';
-  angular.module('myApp', ['ui.router']).config(function($stateProvider, $urlRouterProvider) {
+  angular.module('myApp', ['ui.router'])
+  .controller('ListOneController', function(videos) {
+    this.items = videos;
+  })
+  
+  .config(function($stateProvider, $urlRouterProvider) {
     //
     // For any unmatched url, redirect to /state1
-    $urlRouterProvider.otherwise("/state1");
+    $urlRouterProvider.otherwise('/state1');
     //
     // Now set up the states
     $stateProvider.state('state1', {
-      url: "/state1",
-      templateUrl: "templates/state1.html"
+      url: '/state1',
+      templateUrl: 'templates/state1.html'
     })
-      
-      .state('state1.list', {
-      url: "/list/:query",
-      templateUrl: "templates/state1.list.html",
+
+    .state('state1.list', {
+      url: '/list/:query',
+      templateUrl: 'templates/state1.list.html',
       resolve: {
         videos: function($http, $stateParams) {
-          
-          // http://survivejs.com/common_problems/pyramid.html          
           return $http.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&type=video&q=' + $stateParams.query + '&key=AIzaSyD4YJITOWdfQdFbcxHc6TgeCKmVS9yRuQ8')
             .then(function(res) {
               return res.data.items.map(function(v) {
@@ -34,22 +37,20 @@
       },
       controller: 'ListOneController as list'
     })
-      
-      .state('state2', {
-      url: "/state2",
-      templateUrl: "templates/state2.html"
+
+    .state('state2', {
+      url: '/state2',
+      templateUrl: 'templates/state2.html'
     })
-      
-      .state('state2.list', {
-      url: "/list",
-      templateUrl: "templates/state2.list.html",
+
+    .state('state2.list', {
+      url: '/list',
+      templateUrl: 'templates/state2.list.html',
       controller: function($scope) {
-        $scope.things = ["A", "Set", "Of", "Things"];
+        $scope.things = ['A', 'Set', 'Of', 'Things'];
       }
     });
   })
-    
-    .controller('ListOneController', function(videos) {
-    this.items = videos;
-  })
+
+  
 })();
