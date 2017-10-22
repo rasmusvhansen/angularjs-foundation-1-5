a7f24604edf5551e50299ad960261e11d65e13fc
Rasmus Hansen <rasmusvhansen@gmail.com>
2017-10-22 21:30:40 +0200

    Added prettier config upgraded to latest angular and karma

--- samples/06Services/scripts/app.js
+++ samples/06Services/scripts/app.js
@@ -1,10 +1,9 @@
 (function() {
   'use strict';
-  angular.module('myApp', ['ui.router'])
-  
-  .controller('ListOneController', function(videos) {
-    this.items = videos;
-  })
-  
-  .factory('videoService', function ($http){});
+  angular
+    .module('myApp', ['ui.router'])
+    .controller('ListOneController', function(videos) {
+      this.items = videos;
+    })
+    .factory('videoService', function($http) {});
 })();
--- samples/08Directives/scripts/widget.js
+++ samples/08Directives/scripts/widget.js
@@ -1,23 +1,21 @@
-(function () {
+(function() {
   'use strict';
-  angular.module('myApp', [])
-  
-  .directive('myColorWidget', function ($timeout) {
+  angular.module('myApp', []).directive('myColorWidget', function($timeout) {
     return {
       restrict: 'E',
       template: '<div class="widget"></div>',
-      link: function (_, element, attrs) {
-        element.on('mousemove', function (e) {
+      link: function(_, element, attrs) {
+        element.on('mousemove', function(e) {
           var x = e.clientX;
           var y = e.clientY;
           var el = angular.element('<div class="emitted"></div>');
-          var color = '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
+          var color = '#' + Math.floor(Math.random() * 0xffffff).toString(16);
           el.css('top', y + 'px');
           el.css('left', x + 'px');
           el.css('border-color', color);
           element.append(el);
           $timeout(el.remove.bind(el), 500);
-        })
+        });
       }
     };
   });
