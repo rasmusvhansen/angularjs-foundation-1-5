6210db5240a2381d3e0a9326b57ee7afe930cf9f
Rasmus Vestergaard Hansen <Rasmus.Vestergaard.Hansen@systematic.com>
2017-10-22 22:05:47 +0200

    Updated search query

--- samples/04Components/app.js
+++ samples/04Components/app.js
@@ -1,40 +1,40 @@
-(function() {
+(function () {
   'use strict';
   angular.module('myApp', [])
     .component('videoDetails', {
       templateUrl: 'videoDetails.html',
-      controller: function controller() {},
+      controller: function controller() { },
       bindings: {
         video: '<',
         onShow: '&'
       }
     })
 
-  .component('app', {
-    templateUrl: 'app.html',
-    controller: function($http, $log) {
-      var ctrl = this;
-      ctrl.query = 'Rick';
-      ctrl.videos = [];
+    .component('app', {
+      templateUrl: 'app.html',
+      controller: function ($http, $log) {
+        var ctrl = this;
+        ctrl.query = 'Rick Astley';
+        ctrl.videos = [];
 
-      ctrl.show = function(video) {
-        alert('Showing: ' + video.title);
-      };
+        ctrl.show = function (video) {
+          alert('Showing: ' + video.title);
+        };
 
-      $http.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&type=video&q=' + ctrl.query + '&key=AIzaSyD4YJITOWdfQdFbcxHc6TgeCKmVS9yRuQ8')
-        .then(function(res) {
-          $log.info(res.data.items);
+        $http.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&type=video&q=' + ctrl.query + '&key=AIzaSyD4YJITOWdfQdFbcxHc6TgeCKmVS9yRuQ8')
+          .then(function (res) {
+            $log.info(res.data.items);
 
-          ctrl.videos = res.data.items.map(function(v) {
-            var snip = v.snippet;
-            return {
-              title: snip.title,
-              description: snip.description,
-              thumb: snip.thumbnails.medium.url,
-              link: 'https://www.youtube.com/watch?v=' + v.id.videoId
-            };
+            ctrl.videos = res.data.items.map(function (v) {
+              var snip = v.snippet;
+              return {
+                title: snip.title,
+                description: snip.description,
+                thumb: snip.thumbnails.medium.url,
+                link: 'https://www.youtube.com/watch?v=' + v.id.videoId
+              };
+            });
           });
-        });
-    }
-  });
+      }
+    });
 })();
