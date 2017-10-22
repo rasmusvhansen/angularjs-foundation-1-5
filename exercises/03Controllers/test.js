57e579657405e7a1bae3c0a87e882f55ba8fa50e
Rasmus Vestergaard Hansen <Rasmus.Vestergaard.Hansen@systematic.com>
2017-10-22 22:10:58 +0200

    Fixed typo in comment

--- exercises/03Controllers/test.js
+++ exercises/03Controllers/test.js
@@ -1,56 +1,58 @@
-(function() {
+(function () {
   'use strict';
-  describe('VideoListController', function() {
+  describe('VideoListController', function () {
     var controller,
-        httpBackend;
+      httpBackend;
     beforeEach(module('myApp'));
-    
+
     beforeEach(inject(function ($controller, $httpBackend) {
-      httpBackend = $httpBackend; 
+      httpBackend = $httpBackend;
       controller = $controller('VideoListController');
-      httpBackend.expectGET(/.*q=Rick Astley.*/).respond({items: []});
+      httpBackend.expectGET(/.*q=Rick Astley.*/).respond({ items: [] });
       httpBackend.flush();
     }));
-    
+
     it('should query for Rick Astley by default', function () {
       expect(controller.query).toBe('Rick Astley');
-      
-      httpBackend.expectGET(/.*q=Rick Astley.*/).respond({items: []});
+
+      httpBackend.expectGET(/.*q=Rick Astley.*/).respond({ items: [] });
       controller.search();
       httpBackend.flush();
     });
-    
+
     it('should query for user specified term', function () {
       controller.query = 'Angular';
-      
-      httpBackend.expectGET(/.*q=Angular.*/).respond({items: []});
+
+      httpBackend.expectGET(/.*q=Angular.*/).respond({ items: [] });
       controller.search();
       httpBackend.flush();
     });
-    
+
     it('should map items to proper structure', function () {
       httpBackend.expectGET(/.*q=Rick Astley.*/).respond(
-        {items: [
-          {
-            id: {videoId: 'vidId'},
-            snippet: {
-              title: 'vidTitle',
-              description: 'vidDescription',
-              thumbnails: {
-                medium: {
-                  url: 'thumbUrl'
+        {
+          items: [
+            {
+              id: { videoId: 'vidId' },
+              snippet: {
+                title: 'vidTitle',
+                description: 'vidDescription',
+                thumbnails: {
+                  medium: {
+                    url: 'thumbUrl'
+                  }
                 }
               }
             }
-          }
-        ]});
+          ]
+        });
       controller.search();
-      httpBackend.flush();      
-      
+      httpBackend.flush();
+
       expect(controller.videos.length).toBe(1);
-      
+
       // TODO: verify that ctrl.videos looks as expected given this reply from $http
     });
-        
+
   });
 })();
