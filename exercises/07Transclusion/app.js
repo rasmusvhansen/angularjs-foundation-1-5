6a6cd5ce7fe42e417f24376fa75db070e68c2736
rasmusvhansen <rasmusvhansen@gmail.com>
2017-10-26 11:54:45 +0200

    Added helper text for transclusion casing

--- exercises/07Transclusion/app.js
+++ exercises/07Transclusion/app.js
@@ -3,5 +3,6 @@
   angular.module('myApp', ['ngAnimate'])
     .component('group', {
       templateUrl: 'group.html'
+      // Note that names of transclusion elements must be camel-cased, e.g groupLabel instead of group-label
     });
 })();
